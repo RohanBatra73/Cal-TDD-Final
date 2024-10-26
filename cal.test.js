1fbc8413aec60f36553b424f357249ddb9a6e877
Rohan Batra <rohanbatra73@gmail.com>
2024-10-26 13:11:02 +0530

    Added methods for Comma, multiple commas, & delimi

--- cal.test.js
+++ cal.test.js
@@ -11,4 +11,15 @@ test('returns the number itself for a single number', () => {
 });
 
 
-  
+test('returns the sum of two comma-separated numbers', () => {
+  expect(stringCalculator("1,2")).toBe(3);
+  expect(stringCalculator("4,5")).toBe(9);
+});
+
+test('returns the sum of multiple comma-separated numbers', () => {
+  expect(stringCalculator("1,2,3")).toBe(6);
+});
+// new
+test('handles newlines as delimiters', () => {
+    expect(stringCalculator("1\n2,3")).toBe(6);
+  });
